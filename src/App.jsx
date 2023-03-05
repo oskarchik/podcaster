999badf53729a548b38fa52568814ebe44668893
Oscar Sanz <63589077+oskarchik@users.noreply.github.com>
2023-03-05 20:06:58 +0100

    Feat/routing (#2)
    * feat(app): add routes and Header to App.jsx
    
    * chore(app): install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Header } from './components';
+import { Home, Podcast, Episode } from './pages';
 
 export const App = () => {
-	return <div className='app'>App</div>;
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='podcast/:podcastId' element={<Podcast />} />
+				<Route path='podcast/:podcastId/episode/:episodeId' element={<Episode />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
