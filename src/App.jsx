9376de5fba5e01a53a23440f51b389444668db6d
Oscar Sanz <osanzce@gmail.com>
2023-03-08 18:39:51 +0100

    feat(app): add GlobaStyle to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Header } from './components';
 import { Home, Podcast, Episode } from './pages';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 export const App = () => {
 	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route exact path='/' element={<Home />} />
-				<Route path='podcast/:podcastId' element={<Podcast />} />
-				<Route path='podcast/:podcastId/episode/:episodeId' element={<Episode />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+			<GlobalStyle />
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='podcast/:podcastId' element={<Podcast />} />
+					<Route path='podcast/:podcastId/episode/:episodeId' element={<Episode />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 };
