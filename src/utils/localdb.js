a21c8faa22aba34a0183a46571cdcd501be25b15
Oscar Sanz <osanzce@gmail.com>
2023-03-14 05:14:59 +0100

    feat(localdb): add podcastId collection to browser db

--- src/utils/localdb.js
+++ src/utils/localdb.js
@@ -4,4 +4,5 @@ export const db = new Dexie('podcastsDataBase');
 
 db.version(1).stores({
 	podcasts: 'id, podcasts, timestamp', // Primary key and indexed props
+	podcastId: 'id, data, timestamp',
 });
