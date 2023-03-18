a04bed2c59eb2f9ab2e9962d087cca492475d75f
Oscar Sanz <osanzce@gmail.com>
2023-03-18 11:34:15 +0100

    refactor(controls): create Controls component from AudioPlayer component

---
+++ src/components/Controls/Controlos.styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const StyledControls = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #44433f;
+	gap: 8px;
+	color: white;
+	padding: 0.25rem 0;
+	border-radius: 4px;
+	.progress {
+		width: 100%;
+		height: 8px;
+		}
+		border-radius: 4px;
+	}
+	.btn {
+		display: flex;
+		align-items: center;
+		border-radius: 4px;
+		border: none;
+		background-color: #44433f;
+		color: white;
+		cursor: pointer;
+	}
+`;
---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,48 @@
+import { StyledControls } from './Controlos.styled';
+import { PlayIcon, PauseIcon, VolumeIcon, MuteIcon } from '../../components/Icons/Icons';
+import { calculateTime } from '../../utils/calculateTime';
+import { useRef } from 'react';
+
+export const Controls = ({
+	audioRef,
+	handleMute,
+	handlePlayPause,
+	isMuted,
+	isPlaying,
+	currentTime,
+	currentPercentage,
+}) => {
+	const clickRef = useRef();
+	const checkDuration = (e) => {
+		const width = clickRef.current.clientWidth;
+		const offset = e.nativeEvent.offsetX;
+		const progress = (offset / width) * 100;
+
+		audioRef.current.currentTime = (progress / 100) * audioRef.current.duration;
+	};
+	return (
+		<>
+			{audioRef.current && (
+				<StyledControls currentPercentage={currentPercentage}>
+					<button onClick={handlePlayPause} className='btn'>
+						{isPlaying ? <PauseIcon /> : <PlayIcon />}
+					</button>
+					{/* <div  className='progress-container' > */}
+					<div
+						className='progress'
+						style={{
+							background: `linear-gradient(to right, white ${currentPercentage}%, black 0)`,
+						}}
+						onClick={checkDuration}
+						ref={clickRef}
+					></div>
+					{/* </div> */}
+					<div>{calculateTime(currentTime)}</div>
+					<button onClick={handleMute} className='btn'>
+						{isMuted ? <MuteIcon /> : <VolumeIcon />}
+					</button>
+				</StyledControls>
+			)}
+		</>
+	);
+};
