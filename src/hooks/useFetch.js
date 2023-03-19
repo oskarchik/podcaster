e719f4e12b8d006fd6e1e549a99f9a373d3333d5
Oscar Sanz <osanzce@gmail.com>
2023-03-19 12:35:28 +0100

    refactor(usefetch): refactor useFetch to include getEpisodes logic and reuse hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,45 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLoading } from './useLoading';
-import { db } from '../utils/localdb';
 
-export const useFetch = ({ url }) => {
+export const useFetch = ({ id, collection, url }) => {
 	const [data, setData] = useState();
 	const { setIsLoading } = useLoading();
 
 	useEffect(() => {
 		setIsLoading(true);
 
-		const getPodcasts = async () => {
+		const getResources = async () => {
 			try {
-				const allPodcasts = await db.podcasts
-					.where('id')
-					.belowOrEqual(Date.now() - 24 * 60 * 60 * 1000)
-					.toArray();
-				if (allPodcasts.length > 0) {
-					setData(allPodcasts[0].data);
+				const resources = await collection.where('id').equals(id).toArray();
+
+				if (resources.length > 0 && Date.now() - 24 * 60 * 60 * 1000 <= resources[0].timestamp) {
+					setData(resources[0].data);
 					setIsLoading(false);
-				} else {
-					const fetchData = async () => {
-						try {
-							const response = await fetch(url);
-							const items = await response.json();
-
-							await db.podcasts.put({ id: 1, data: items.feed.entry, timestamp: Date.now() });
-							setData(items.feed?.entry);
-
-							setIsLoading(false);
-						} catch (error) {
-							console.error(error.stack);
-						}
-					};
-					fetchData();
+					return;
 				}
+				const fetchData = async () => {
+					try {
+						const response = await fetch(url);
+						const data = await response.json();
+
+						const parsedData = data.feed ? data.feed.entry : await JSON.parse(data.contents);
+
+						const returnData = parsedData.results ? parsedData.results.slice(1) : parsedData;
+
+						await collection.put({
+							id,
+							data: returnData,
+							timestamp: Date.now(),
+						});
+						setData(returnData);
+						setIsLoading(false);
+					} catch (error) {
+						console.log(error);
+					}
+				};
+				fetchData();
 			} catch (error) {
 				console.log(error);
 			}
 		};
-
-		getPodcasts();
+		getResources();
 	}, []);
 
 	return { data, setData };
